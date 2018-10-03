8b4af01e1f9c4ea474ef3bec11a55d44fe39a901
KaluGN <kaluguzmann@gmail.com>
2018-10-03 15:39:39 -0500

    estructura

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCZ04vTuzzAYoaEE59nJ9cTD_U2QoY_FhM",
-    authDomain: "you-work-kalu.firebaseapp.com",
-    databaseURL: "https://you-work-kalu.firebaseio.com",
-    projectId: "you-work-kalu",
-    storageBucket: "you-work-kalu.appspot.com",
-    messagingSenderId: "737582515467"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCZ04vTuzzAYoaEE59nJ9cTD_U2QoY_FhM",
+  authDomain: "you-work-kalu.firebaseapp.com",
+  databaseURL: "https://you-work-kalu.firebaseio.com",
+  projectId: "you-work-kalu",
+  storageBucket: "you-work-kalu.appspot.com",
+  messagingSenderId: "737582515467"
+};
+firebase.initializeApp(config);
