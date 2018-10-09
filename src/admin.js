17fc30d8092440dd5b0fb3bf40b922b54f488521
KaluGN <kaluguzmann@gmail.com>
2018-10-09 18:07:21 -0500

    sal toda la lista de visitantes

--- src/admin.js
+++ src/admin.js
@@ -4,10 +4,11 @@ const viewVisit = () => {
   let reference = firebase.database().ref('/registro');
   reference.once('value', (data) => {
     data.forEach(registro => {
-      let visitor = registro.key,
+      console.log(registro.val())
+      let visitors = registro.key,
       registroDatos = registro.val();
-      contentTable.innerHTML = `
-          <tr data-key="${visitor}">
+      contentTable.innerHTML += `
+          <tr data-key="${visitors}">
             <td>${registroDatos.name}</td>
             <td>${registroDatos.apellido}</td>
             <td>${registroDatos.empresa}</td>
